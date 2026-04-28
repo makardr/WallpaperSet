174cff70a1c689145c77937226c8f3f02183ae9a
makardr <makardrozdik00@gmail.com>
2026-04-28 17:36:53 +0300

    Interface improvements, bug fixes

--- app/src/main/java/com/example/wallpaperfix/MainActivity.kt
+++ app/src/main/java/com/example/wallpaperfix/MainActivity.kt
@@ -3,27 +3,25 @@ package com.example.wallpaperfix
 import android.app.WallpaperManager
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Rect
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import com.yalantis.ucrop.UCrop
 import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
-    private val debugTag = "WallpaperFixDebug";
+    private val debugTag = "WallpaperFixDebug"
+    private val imageUriDebug = "imageUriDebug"
     private val wallpaperCacheFileName = "cropped_wallpaper_cache.jpg"
     private lateinit var imageCacheOutputUri: Uri
     private lateinit var wallpaperPreview: ImageView
@@ -31,18 +29,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var setWallpaperLock: Button
     private lateinit var setWallpaperAll: Button
 
+    private lateinit var cropImageButton: Button
+
     private var imageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        setupInterface()
+        Log.d(debugTag, "App created")
+        setupInterface(savedInstanceState)
 
         imageCacheOutputUri = Uri.fromFile(File(cacheDir, wallpaperCacheFileName))
 
@@ -51,12 +47,12 @@ class MainActivity : AppCompatActivity() {
             handleIncomingIntent(intent)
         }
 
-        imageUri?.let {
-            launchUCropActivity(it)
-        }
-
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable("imageUri", imageUri)
+    }
 
     private fun launchUCropActivity(uri: Uri) {
 
@@ -88,7 +84,9 @@ class MainActivity : AppCompatActivity() {
             RESULT_OK -> {
                 val croppedUri = UCrop.getOutput(result.data!!)
                 imageUri = croppedUri
-                Log.d(tag, imageUri.toString())
+                wallpaperPreview.setImageURI(null)
+                wallpaperPreview.setImageURI(imageUri)
+                Log.d(imageUriDebug, "cropResultLauncher set imageUri as $imageUri")
 //                croppedUri?.let { setWallpaper(it, WallpaperManager.FLAG_SYSTEM) }
             }
 
@@ -123,14 +121,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun handleImageGeneric(intent: Intent) {
-        val tag = "handleImageGeneric";
+        val tag = "handleImageGeneric"
         val sharedUri: Uri? = intent.data
         Log.d(tag, sharedUri.toString())
-        6
         imageUri = sharedUri
-        imageUri?.let {
-            launchUCropActivity(it)
-        }
+        wallpaperPreview.setImageURI(imageUri)
+        Log.d(imageUriDebug, "handleImageGeneric set uri as $imageUri")
     }
 
 
@@ -157,12 +153,14 @@ class MainActivity : AppCompatActivity() {
         Log.d(tag, "Wallpaper applied")
     }
 
-    private fun setupInterface() {
+    private fun setupInterface(savedInstanceState: Bundle?) {
+        val tag = "setupInterface"
         wallpaperPreview = findViewById(R.id.wallpaperPreview)
 
         setWallpaperSystem = findViewById(R.id.setWallpaperSystem)
         setWallpaperLock = findViewById(R.id.setWallpaperLock)
         setWallpaperAll = findViewById(R.id.setWallpaperAll)
+        cropImageButton = findViewById(R.id.cropImage)
 
         setWallpaperSystem.setOnClickListener {
             imageUri?.let {
@@ -180,6 +178,60 @@ class MainActivity : AppCompatActivity() {
             imageUri?.let {
                 setWallpaper(it, WallpaperManager.FLAG_SYSTEM or WallpaperManager.FLAG_LOCK)
             }
+        }
+
+        cropImageButton.setOnClickListener {
+            imageUri?.let {
+                launchUCropActivity(it)
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(setWallpaperSystem) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                bottomMargin = systemBars.bottom
+            }
+            insets
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(setWallpaperLock) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                bottomMargin = systemBars.bottom
+            }
+            insets
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(setWallpaperAll) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                bottomMargin = systemBars.bottom
+            }
+            insets
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(cropImageButton) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                topMargin = systemBars.top
+            }
+            insets
+        }
+
+
+
+
+        if (savedInstanceState != null) {
+            imageUri =
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+                    savedInstanceState.getParcelable("imageUri", Uri::class.java)
+                } else {
+                    @Suppress("DEPRECATION")
+                    savedInstanceState.getParcelable("imageUri")
+                }
+            imageUri?.let {
+                wallpaperPreview.setImageURI(it)
+            }
+            Log.d(imageUriDebug, "setupInterface onCreate savedImageUri as $imageUri")
         }
     }
 }
