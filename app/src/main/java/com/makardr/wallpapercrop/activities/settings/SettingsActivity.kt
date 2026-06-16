aa59629d27798b5e1149b1a9a76c506e5f4e5e95
makardr <makardrozdik00@gmail.com>
2026-06-16 16:59:01 +0300

    Created ImageStorageManager

--- app/src/main/java/com/makardr/wallpapercrop/activities/settings/SettingsActivity.kt
+++ app/src/main/java/com/makardr/wallpapercrop/activities/settings/SettingsActivity.kt
@@ -1,15 +1,24 @@
 package com.makardr.wallpapercrop.activities.settings
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.makardr.wallpapercrop.R
+import com.makardr.wallpapercrop.common.utils.isTablet
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupInterface()
+    }
+
+    fun setupInterface(){
+        if (!isTablet()) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
